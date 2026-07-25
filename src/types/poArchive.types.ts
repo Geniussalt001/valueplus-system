77ad2386ce1ee9d9bb5d318884cb795edf3d9155
Geniussalt001 <rachata_sbac38156@hotmail.com>
2026-07-25 09:16:04 +0700

    Add PO document archive types

---
+++ src/types/poArchive.types.ts
@@ -0,0 +1,45 @@
+export type PoArchiveStatus =
+  | "stored"
+  | "duplicate";
+
+export interface PoArchiveRecord {
+  id: string;
+  poNumber: string;
+  documentDate: string;
+  warehouse: string;
+  fileId: string;
+  fileName: string;
+  fileUrl: string;
+  fileSize: number;
+  uploadedAt: string;
+  uploadedBy: string;
+  status: PoArchiveStatus;
+  note: string;
+}
+
+export interface LocalPdfData {
+  fileName: string;
+  base64Data: string;
+  size: number;
+}
+
+export interface UploadPoArchiveInput {
+  poNumber: string;
+  documentDate: string;
+  warehouse: string;
+  fileName: string;
+  base64Data: string;
+}
+
+export interface PoArchivePdf {
+  fileId: string;
+  fileName: string;
+  mimeType: "application/pdf";
+  base64Data: string;
+}
+
+export interface PoArchiveUploadResult {
+  status: PoArchiveStatus;
+  message: string;
+  record: PoArchiveRecord;
+}
