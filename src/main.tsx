7b7b89f4064290a26ce89e2a7f7bc491de531e8e
Geniussalt001 <rachata_sbac38156@hotmail.com>
2026-08-24 14:22:44 +0700

    Unify system colors with Product Spotlight

--- src/main.tsx
+++ src/main.tsx
@@ -9,6 +9,7 @@ import "./styles/blue-dashboard-theme.css";
 import "./styles/valueplus-red-theme.css";
 import "./styles/valueplus-modern-theme.css";
 import "./styles/product-showcase.css";
+import "./styles/showcase-system-theme.css";
 
 import App from "./App";
 
@@ -26,3 +27,4 @@ ReactDOM.createRoot(rootElement).render(
     <App />
   </React.StrictMode>,
 );
+
