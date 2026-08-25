6d668f0419a8615d3c190f641233fde0435f8bff
Geniussalt001 <rachata_sbac38156@hotmail.com>
2026-08-25 14:50:26 +0700

    fix: support new CP ALL PDF format in python/valueplus_common/__init__.py

--- python/valueplus_common/__init__.py
+++ python/valueplus_common/__init__.py
@@ -1,7 +1,13 @@
 """Shared helpers used by multiple ValuePlus engines."""
 
-from .cpall_pdf import normalize_wrapped_item_quantities
+from .cpall_pdf import (
+    normalize_cpall_document_date,
+    normalize_wrapped_item_quantities,
+    repair_cpall_extracted_text,
+)
 
 __all__ = [
+    "normalize_cpall_document_date",
     "normalize_wrapped_item_quantities",
+    "repair_cpall_extracted_text",
 ]
